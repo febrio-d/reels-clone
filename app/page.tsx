d330790d3a4c17d880564eaf2ffc2fab9e5e4105
febrio-d <7g26febrio@gmail.com>
2024-05-02 13:33:08 +0700

    add video section

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { IoMusicalNotes } from "react-icons/io5";
 import { MdLibraryMusic, MdVideoLibrary } from "react-icons/md";
 import { PiPaperPlaneTiltLight } from "react-icons/pi";
 import { RiSearchLine } from "react-icons/ri";
+// import Vid1 from "@/assets/vid-1.mp4";
 
 export default function Home() {
   return (
@@ -21,16 +22,114 @@ export default function Home() {
           <p className="font-medium">Reels</p>
           <FiCamera className="size-6" />
         </div>
-        <div className="flex-1 h-[100vh-72px]">
+        <div className="flex-1 h-[calc(100vh-72px)]">
           <div className="h-full w-full box-border">
-            <div className="mx-auto relative overflow-hidden p-0 h-full">
+            <div className="mx-auto relative overflow-y-scroll no-scrollbar scroll-snap-y p-0 h-full">
               {/* {[...Array(5)].map((_, i) => ( */}
               {/* key={i.toString()} */}
-              <div className="relative flex flex-col transition-[transform,height] h-full transform">
+              <div className="relative flex scroll-snap-align-start flex-col h-full">
                 <video
                   loop
-                  className="w-full relative h-full"
-                  src="https://drive.google.com/uc?export=download&id=1NmGuxh6l3SXBNdn5ZoSm5x6gDpNAs2nz/"
+                  autoPlay
+                  // muted
+                  playsInline
+                  preload="none"
+                  className="w-full relative object-fill h-full"
+                  src="https://cloud.appwrite.io/v1/storage/buckets/655188de6cf0c74e18cf/files/pz5qlcz8ofp/view?project=65518312cabec3b116e6"
+                />
+                <div className="bottom-0 z-10 left-0 absolute pb-3 ml-4 flex flex-col gap-1">
+                  <div className="flex gap-3 items-center">
+                    <div className="rounded-full bg-white size-6"></div>
+                    <p>oke</p>
+                  </div>
+                  <p className="text-sm">#edukasi #fyp #finansial</p>
+                  <div className="bg-slate-500/60 flex items-center gap-2 text-xs rounded-3xl px-2 py-0.5">
+                    <IoMusicalNotes className="size-3" />
+                    <p>awesome - Original audio</p>
+                  </div>
+                </div>
+                <div className="bottom-0 z-10 right-0 absolute pb-3 mr-3 flex flex-col gap-4">
+                  {/* <p>L</p> */}
+                  <div className="flex flex-col gap-1 items-center">
+                    <AiOutlineHeart className="size-7" />
+                    <p className="text-xs">0</p>
+                  </div>
+                  {/* <p>C</p> */}
+                  <div className="flex flex-col gap-1 items-center">
+                    <AiOutlineComment className="size-7" />
+                    <p className="text-xs">0</p>
+                  </div>
+                  {/* <p>S</p> */}
+                  <div className="flex flex-col gap-1 items-center">
+                    <PiPaperPlaneTiltLight className="size-7" />
+                    <p className="text-xs">0</p>
+                  </div>
+                  {/* <p>E</p> */}
+                  <div className="flex flex-col gap-1 items-center">
+                    <HiOutlineDotsHorizontal className="size-5" />
+                  </div>
+                  {/* <p>M</p> */}
+                  <div className="flex flex-col gap-1 items-center">
+                    <MdLibraryMusic className="size-7" />
+                  </div>
+                </div>
+              </div>
+              <div className="relative flex scroll-snap-align-start flex-col h-full">
+                <video
+                  loop
+                  autoPlay={true}
+                  muted
+                  playsInline
+                  preload="none"
+                  className="w-full relative object-fill h-full"
+                  src="/vid-1.mp4"
+                />
+                <div className="bottom-0 z-10 left-0 absolute pb-3 ml-4 flex flex-col gap-1">
+                  <div className="flex gap-3 items-center">
+                    <div className="rounded-full bg-white size-6"></div>
+                    <p>oke</p>
+                  </div>
+                  <p className="text-sm">#edukasi #fyp #finansial</p>
+                  <div className="bg-slate-500/60 flex items-center gap-2 text-xs rounded-3xl px-2 py-0.5">
+                    <IoMusicalNotes className="size-3" />
+                    <p>awesome - Original audio</p>
+                  </div>
+                </div>
+                <div className="bottom-0 z-10 right-0 absolute pb-3 mr-3 flex flex-col gap-4">
+                  {/* <p>L</p> */}
+                  <div className="flex flex-col gap-1 items-center">
+                    <AiOutlineHeart className="size-7" />
+                    <p className="text-xs">0</p>
+                  </div>
+                  {/* <p>C</p> */}
+                  <div className="flex flex-col gap-1 items-center">
+                    <AiOutlineComment className="size-7" />
+                    <p className="text-xs">0</p>
+                  </div>
+                  {/* <p>S</p> */}
+                  <div className="flex flex-col gap-1 items-center">
+                    <PiPaperPlaneTiltLight className="size-7" />
+                    <p className="text-xs">0</p>
+                  </div>
+                  {/* <p>E</p> */}
+                  <div className="flex flex-col gap-1 items-center">
+                    <HiOutlineDotsHorizontal className="size-5" />
+                  </div>
+                  {/* <p>M</p> */}
+                  <div className="flex flex-col gap-1 items-center">
+                    <MdLibraryMusic className="size-7" />
+                  </div>
+                </div>
+              </div>
+              <div className="relative flex scroll-snap-align-start flex-col h-full">
+                <video
+                  loop
+                  autoPlay={true}
+                  muted
+                  playsInline
+                  preload="none"
+                  className="w-full relative object-fill h-full"
+                  src="/vid-1.mp4"
                 />
                 <div className="bottom-0 z-10 left-0 absolute pb-3 ml-4 flex flex-col gap-1">
                   <div className="flex gap-3 items-center">
@@ -75,28 +174,28 @@ export default function Home() {
         </div>
         <div
           className={cn(
-            "grid grid-cols-5 bg-black justify-center pb-10 pt-2 items-center"
+            "grid grid-cols-5 bg-black justify-center z-20 pb-5 pt-3 items-center"
             // , "rounded-b-xl"
           )}
         >
           <button type="button" className={cn("text-white w-full")}>
-            <GoHome className="size-6 m-auto" />
+            <GoHome className="size-5 m-auto" />
             {/* Home */}
           </button>
           <button type="button" className={cn("text-white w-full")}>
-            <RiSearchLine className="size-6 m-auto" />
+            <RiSearchLine className="size-5 m-auto" />
             {/* Search */}
           </button>
           <button type="button" className={cn("text-white w-full")}>
-            <FaRegPlusSquare className="size-6 m-auto" />
+            <FaRegPlusSquare className="size-5 m-auto" />
             {/* Add */}
           </button>
           <button type="button" className={cn("text-white w-full")}>
-            <MdVideoLibrary className="size-6 m-auto" />
+            <MdVideoLibrary className="size-5 m-auto" />
             {/* Reels */}
           </button>
           <button type="button" className={cn("text-white w-full")}>
-            <div className="rounded-full m-auto bg-white size-6"></div>
+            <div className="rounded-full m-auto bg-white size-5"></div>
             {/* Profile */}
           </button>
         </div>
